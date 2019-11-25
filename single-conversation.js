8dc1775e98773710dc02b74a7218e3fc82f854d3
barrelmaker <cooperleong0@gmail.com>
2019-11-25 17:54:14 -0500

    Add getContact function

--- single-conversation.js
+++ single-conversation.js
@@ -6,16 +6,33 @@ const app = express();
 port = 3000
 node_env = 'development'
 
+// Opens up a stdin raw input
+
+
 // Grabs the arguments
 const args = process.argv.slice(2);
 console.log('args   : ', args);
 
+function getContact() {
+
+  // Create readline instance
+  const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+
+  readline.question(`Contact: `, (contact) => {
+    contact = `${contact}`
+    // Close the readline input
+    readline.close()
+    sendMessage(contact)
+  });
+}
+
 // Function that takes raw input message and sends it to the contact
-function sendMessage() {
-  // Contact is the first argument in the command
-  contact = args[0]
-  
-  // Opens up a stdin raw input
+function sendMessage(contact) {
+
+  // Create readline instance
   const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
@@ -24,7 +41,7 @@ function sendMessage() {
   // Opens the readline
   readline.question(`Message: `, (message) => {
     message = `${message}`
-
+    console.log(message)
     // Creates the string that runs the applescript
     const script = `tell application "Messages" to send "${message}" to buddy "${contact}"`;
     
@@ -39,14 +56,24 @@ function sendMessage() {
     readline.close()
 
     // Call the sendMessage() function again to send another message
-    sendMessage();
+    sendMessage(contact);
   })
 }
+
+/* Add function to check for incoming messages, will most likely need to be integrated with chat.db
+function test() {
+  console.log('test')
+}
+*/
 
 module.exports = app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
   console.log(`Environment: ${node_env}`);
 
+  getContact()
   // Calls the sendMessage() function
-  sendMessage()
+  //sendMessage()
+
+  // Change to have it checking for incoming messages from that contact
+  //test()
 });
